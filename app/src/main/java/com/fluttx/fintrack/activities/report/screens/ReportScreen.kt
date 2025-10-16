7f9f8a9f1a00b89554e8231b411721316bb0fb2a
alamin-karno <alamin.karno@gmail.com>
2025-10-16 19:14:46 +0600

    feat: add SummaryColumns component and integrate it into ReportScreen

--- app/src/main/java/com/fluttx/fintrack/activities/report/screens/ReportScreen.kt
+++ app/src/main/java/com/fluttx/fintrack/activities/report/screens/ReportScreen.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.fluttx.fintrack.R
 import com.fluttx.fintrack.activities.report.components.CenterStatsCard
 import com.fluttx.fintrack.activities.report.components.GradientHeader
+import com.fluttx.fintrack.activities.report.components.SummaryColumns
 import com.fluttx.fintrack.domain.BudgetDomain
 
 @Composable
@@ -80,6 +84,19 @@ fun ReportContent(
                         }
                 )
             }
+        }
+
+        item {
+            SummaryColumns(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .background(
+                        color = colorResource(R.color.lightBlue),
+                        shape = RoundedCornerShape(16.dp),
+                    )
+                    .padding(8.dp)
+            )
         }
     }
 }
